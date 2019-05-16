2d411673813a20cb7a68e77097fb57def3791002
ScottHuangNYU <zh968@nyu.edu>
2019-05-16 17:01:11 -0400

    change the main

--- ExcelCharToInteger.java
+++ ExcelCharToInteger.java
@@ -12,6 +12,6 @@ public class ExcelCharToInteger {
     }
 
     public static void main(String[] args){
-        System.out.println(titleToNumber("A"));
+        System.out.println(titleToNumber("B"));
     }
 }
